3806ae685ad6bc82e07f3f373046cd1726f3a5e7
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2013-01-17 12:21:49 +0100

    + distinct_02.sql

---
+++ SQL/select/distinct/distinct_02.sql
@@ -0,0 +1,38 @@
+create table tq84_distinct (
+       a     number,
+       b     number,
+       c     number
+);
+
+insert into tq84_distinct values (1, 1, 1);
+insert into tq84_distinct values (1, 1, 1);
+insert into tq84_distinct values (1, 1, 1);
+
+insert into tq84_distinct values (2, 2, 2);
+insert into tq84_distinct values (2, 2, 3);
+
+insert into tq84_distinct values (3, 3, 3);
+insert into tq84_distinct values (3, 4, 4);
+
+insert into tq84_distinct values (4, 1, 2);
+insert into tq84_distinct values (4, 3, 4);
+insert into tq84_distinct values (4, 5, 6);
+
+-- Note: the paranthesis around the `a` is
+--       misleading. They belong to the `a`,
+--       not the the distinct.
+
+select distinct (a),
+       b,
+       c
+  from tq84_distinct;
+
+prompt
+
+select distinct 
+       a,
+       b,
+       c
+  from tq84_distinct;
+
+drop table tq84_distinct purge;
