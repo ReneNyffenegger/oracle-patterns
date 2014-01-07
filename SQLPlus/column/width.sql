9c63a67903cb15dfe592733400f27c61a5cd8119
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-01-07 07:55:49 +0100

    + width.sql

---
+++ SQLPlus/column/width.sql
@@ -0,0 +1,17 @@
+create table tq84_column_width (
+  col_1        number,
+  col_2        varchar2(500)
+);
+
+insert into tq84_column_width values (1.049090920, 'alkdjfaj ladjkj kjnvoak zoijuwjd zoijd gh\amd');
+insert into tq84_column_width values (7489899.409, '*132'                                         );
+
+
+column col_1   format  999.99
+column col_2   format  a30
+
+
+select * from tq84_column_width;
+
+drop table tq84_column_width purge;
+
