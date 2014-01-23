93bdaf8ef80211142601b173d6f00d7706a0418e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-01-23 15:11:00 +0100

    Another example

--- SQL/functions/regular_expressions/regexp_substr.sql
+++ SQL/functions/regular_expressions/regexp_substr.sql
@@ -5,8 +5,30 @@ create table tq84_regexp_substr (
 insert into tq84_regexp_substr values ('abc;def;ghi;klm');
 
 -- Extract the third value
-select       regexp_substr(col_1, '[^;]+[^;]', 1, 3)       from tq84_regexp_substr;
+select       regexp_substr(col_1, '[^;]+[^;]', 1, 3)       third from tq84_regexp_substr;
 
-select rtrim(regexp_substr(col_1, '.*?;'     , 1, 3), ';') from tq84_regexp_substr;
+select rtrim(regexp_substr(col_1, '.*?;'     , 1, 3), ';') third from tq84_regexp_substr;
 
 drop table tq84_regexp_substr purge;
+
+--
+
+declare
+
+  -- Get «word» between > and <
+
+  str  varchar2(100) := 'foo bar >words and such< baz';
+
+begin
+
+   dbms_output.put_line(regexp_substr(
+    
+      str,
+     '[^><]+',
+      1,
+      2
+
+   ));
+
+end;
+/
