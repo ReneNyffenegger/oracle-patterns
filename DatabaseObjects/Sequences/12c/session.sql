d6f0f022ac7d9023b77d95a5bdeb3ffa5c2a44fb
Myself <me@me.com>
2014-03-20 15:32:36 +0100

    12 - session sequences

---
+++ DatabaseObjects/Sequences/12c/session.sql
@@ -0,0 +1,17 @@
+create sequence session_seq start with 1 increment by 1 
+   /* Note the following keyword: */ SESSION;
+
+select session_seq.nextval from dual;
+select session_seq.nextval from dual;
+select session_seq.nextval from dual;
+select session_seq.nextval from dual;
+
+
+connect &username/&password
+
+select session_seq.nextval from dual;
+select session_seq.nextval from dual;
+select session_seq.nextval from dual;
+select session_seq.nextval from dual;
+
+drop sequence session_seq;
