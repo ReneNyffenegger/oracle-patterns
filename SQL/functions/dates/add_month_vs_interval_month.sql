34859bf232b5ac12ffd3cb74c42aa594188e4f22
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-03 09:37:23 +0200

    + ORA-01839

--- SQL/functions/dates/add_month_vs_interval_month.sql
+++ SQL/functions/dates/add_month_vs_interval_month.sql
@@ -9,7 +9,10 @@
 alter session set nls_date_format = 'dd.mm.yyyy';
 
 select
-  add_months(date '2014-09-30',            6)        "add_months",  -- 31.03.2015
-             date '2014-09-30' + interval '6' month  "interval"     -- 30.03.2015
+  add_months(date '2014-09-30',            6)        "add_months",     -- 31.03.2015
+             date '2014-09-30' + interval '6' month  "interval",       -- 30.03.2015
+  --
+  add_months(date '2014-03-31',            6)        "add_months 2"    -- 30.09.2014
+--           date '2014-03-31' + interval '6' month  "interval j2"     -- ORA-01839: date not valid for month specified
 from
   dual;
