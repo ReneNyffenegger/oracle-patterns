6523ed60cd8cd07747fbe5d02df7105f2ed18a8f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-02-04 13:42:46 +0100

    set define off so that >nul 2>&1 works

--- Installed/dbms/datapump/clone_schema/cleanup.sql
+++ Installed/dbms/datapump/clone_schema/cleanup.sql
@@ -12,6 +12,11 @@ connect rene/rene
 -- https://github.com/ReneNyffenegger/oracle_scriptlets/blob/master/sqlpath/drop_if_exists.sql
 @drop_if_exists DATAPUMP_DIR
 
-$@del c:\temp\CLONE_SCHEMA_A.DMP        >nul 2>&1
-$@del c:\temp\clone_schema_a_export.log >nul 2>&1
-$@del c:\temp\clone_schema_a_import.log >nul 2>&1
+set define off
+--Setting define off so that 2>&1 doesn't do stupid things...
+
+$del c:\temp\CLONE_SCHEMA_A.DMP        >nul 2>&1
+$del c:\temp\clone_schema_a_export.log >nul 2>&1
+$del c:\temp\clone_schema_a_import.log >nul 2>&1
+
+set define on
