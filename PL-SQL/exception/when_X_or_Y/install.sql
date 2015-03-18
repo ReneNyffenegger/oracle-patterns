4d889e683a2e2e78f7e00f0710fe2630668c18ff
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-03-18 08:13:41 +0100

    when_X_or_Y

---
+++ PL-SQL/exception/when_X_or_Y/install.sql
@@ -0,0 +1,65 @@
+create or replace package tq84_foo as
+
+    exc_one   exception;
+    exc_two   exception;
+    
+    pragma exception_init(exc_one, -20731);
+    pragma exception_init(exc_two, -20732);
+
+    procedure throw_up(x in number);
+
+end tq84_foo;
+/
+
+create or replace package body tq84_foo as
+
+    procedure throw_up(x in number) is
+    begin
+
+        if x = 1 then
+           raise_application_error(-20731, 'Exception one!');
+        end if;
+
+        if x = 2 then
+           raise_application_error(-20732, 'Exception two!');
+        end if;
+
+        raise_application_error(-20800, 'Other error');
+
+    end throw_up;
+
+end tq84_foo;
+/
+
+create or replace package tq84_bar as
+
+    procedure proc(x in number);
+
+end tq84_bar;
+/
+
+create or replace package body tq84_bar as
+
+    procedure proc(x in number) is
+    begin
+
+        tq84_foo.throw_up(x);
+
+    exception
+
+        when tq84_foo.exc_one or tq84_foo.exc_two then
+
+             dbms_output.put_line('caught ' || sqlerrm);
+
+             if sqlcode = -20731 then
+                dbms_output.put_line('  Special Treatment for Exception One!!!');
+             end if;
+
+        when others then
+        
+             raise;
+
+    end proc;
+
+end tq84_bar;
+/
---
+++ PL-SQL/exception/when_X_or_Y/run.sql
@@ -0,0 +1,8 @@
+declare
+begin
+
+  tq84_bar.proc(1);
+  tq84_bar.proc(2);
+
+end;
+/
