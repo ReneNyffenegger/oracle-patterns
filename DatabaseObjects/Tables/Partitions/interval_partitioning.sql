42214c013eea3624c942ad97d185a2a02dc0c33b
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-04-07 13:28:43 +0200

    Add high_value in select statement

--- DatabaseObjects/Tables/Partitions/interval_partitioning.sql
+++ DatabaseObjects/Tables/Partitions/interval_partitioning.sql
@@ -11,19 +11,20 @@ interval (numtoyminterval(1, 'month')) (
   partition tq84_partition_1 values less than (date '2010-01-01')  
 );
 
+column high_value format a100
 
-select partition_name from user_tab_partitions where table_name = 'TQ84_INTERVAL_PARTITION';
+select partition_name, high_value from user_tab_partitions where table_name = 'TQ84_INTERVAL_PARTITION';
 
 insert into tq84_interval_partition values (1, 'Part 1', date '1888-10-18');
-select partition_name from user_tab_partitions where table_name = 'TQ84_INTERVAL_PARTITION';
+select partition_name, high_value from user_tab_partitions where table_name = 'TQ84_INTERVAL_PARTITION';
 
 insert into tq84_interval_partition values (2, 'New part', date '2010-08-13');
-select partition_name from user_tab_partitions where table_name = 'TQ84_INTERVAL_PARTITION';
+select partition_name, high_value from user_tab_partitions where table_name = 'TQ84_INTERVAL_PARTITION';
 
 insert into tq84_interval_partition values (3, 'same part', date '2010-08-12');
-select partition_name from user_tab_partitions where table_name = 'TQ84_INTERVAL_PARTITION';
+select partition_name, high_value from user_tab_partitions where table_name = 'TQ84_INTERVAL_PARTITION';
 
 insert into tq84_interval_partition values (4, 'anoth part', date '2011-02-20');
-select partition_name from user_tab_partitions where table_name = 'TQ84_INTERVAL_PARTITION';
+select partition_name, high_value from user_tab_partitions where table_name = 'TQ84_INTERVAL_PARTITION';
 
 drop table tq84_interval_partition purge;
