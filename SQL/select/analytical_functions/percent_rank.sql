4dead7e56a7b7ecd0f5ef05a217bc8a29995d5b1
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-04 12:07:41 +0200

    Add "aggregate syntax" example.

--- SQL/select/analytical_functions/percent_rank.sql
+++ SQL/select/analytical_functions/percent_rank.sql
@@ -27,4 +27,22 @@ order by
 --          9         .6           .6
 --         10          1            1
 
+select
+  percent_rank( 0  ) within group (order by num) pct_rnk__0,
+  percent_rank( 0.9) within group (order by num) pct_rnk__0_9,
+  percent_rank( 1.0) within group (order by num) pct_rnk__1_0,
+  percent_rank( 1.1) within group (order by num) pct_rnk__1_1,
+  percent_rank( 2  ) within group (order by num) pct_rnk__2,
+  percent_rank( 3  ) within group (order by num) pct_rnk__3,
+  percent_rank( 4  ) within group (order by num) pct_rnk__4,
+  percent_rank(10  ) within group (order by num) pct_rnk_10,
+  percent_rank(11  ) within group (order by num) pct_rnk_11
+from
+  tq84_percent;
+--
+-- PCT_RNK__0 PCT_RNK__0_9 PCT_RNK__1_0 PCT_RNK__1_1 PCT_RNK__2 PCT_RNK__3 PCT_RNK__4 PCT_RNK_10 PCT_RNK_11
+-- ---------- ------------ ------------ ------------ ---------- ---------- ---------- ---------- ----------
+--          0            0            0   .166666667 .166666667 .333333333 .333333333 .833333333          1
+
+
 drop table tq84_percent;
