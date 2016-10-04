bfdf29fe45213297833fd52d87de1b01784088cf
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-04 12:01:22 +0200

    Use rank rather than dense_rank

--- SQL/select/analytical_functions/percent_rank.sql
+++ SQL/select/analytical_functions/percent_rank.sql
@@ -13,10 +13,18 @@ insert into tq84_percent values (10);
 select
     num,
     percent_rank() over (order by num)                               pct_rnk,
-   (dense_rank  () over (order by num) - 1) / (count(*) over ()  -1) pct_rnk_calc
+   (        rank() over (order by num) - 1) / (count(*) over ()  -1) pct_rnk_calc
 from
   tq84_percent
 order by
     num;
+--        NUM    PCT_RNK PCT_RNK_CALC
+-- ---------- ---------- ------------
+--          1          0            0
+--          2         .2           .2
+--          4         .4           .4
+--          9         .6           .6
+--          9         .6           .6
+--         10          1            1
 
 drop table tq84_percent;
