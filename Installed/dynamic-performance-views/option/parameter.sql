127ccd3c0ee89b26636da3378265a05ea3fe5b44
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-12 11:18:39 +0200

    + v$parameter

---
+++ Installed/dynamic-performance-views/option/parameter.sql
@@ -0,0 +1,71 @@
+select
+  parameter
+from
+  v$option
+order by
+  parameter;
+-- Active Data Guard
+-- Advanced Compression
+-- Advanced replication
+-- Application Role
+-- Automatic Storage Management
+-- Backup Encryption
+-- Basic Compression
+-- Bit-mapped indexes
+-- Block Change Tracking
+-- Block Media Recovery
+-- Change Data Capture
+-- Coalesce Index
+-- Connection multiplexing
+-- Connection pooling
+-- DICOM
+-- Data Mining
+-- Database queuing
+-- Database resource manager
+-- Deferred Segment Creation
+-- Duplexed backups
+-- Enterprise User Security
+-- Export transportable tablespaces
+-- Fast-Start Fault Recovery
+-- File Mapping
+-- Fine-grained Auditing
+-- Fine-grained access control
+-- Flashback Data Archive
+-- Flashback Database
+-- Flashback Table
+-- Incremental backup and recovery
+-- Instead-of triggers
+-- Java
+-- Join index
+-- Managed Standby
+-- Materialized view rewrite
+-- Materialized view warehouse refresh
+-- OLAP
+-- OLAP Window Functions
+-- Objects
+-- Online Index Build
+-- Online Redefinition
+-- Oracle Data Guard
+-- Oracle Database Vault
+-- Oracle Label Security
+-- Parallel backup and recovery
+-- Parallel execution
+-- Parallel load
+-- Partitioning
+-- Plan Stability
+-- Point-in-time tablespace recovery
+-- Proxy authentication/authorization
+-- Real Application Clusters
+-- Real Application Testing
+-- Result Cache
+-- SQL Plan Management
+-- Sample Scan
+-- SecureFiles Encryption
+-- Server Flash Cache
+-- Spatial
+-- Streams Capture
+-- Transparent Application Failover
+-- Transparent Data Encryption
+-- Trial Recovery
+-- Unused Block Compression
+-- XStream
