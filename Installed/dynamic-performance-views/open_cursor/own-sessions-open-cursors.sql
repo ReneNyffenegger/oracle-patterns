96f2e6839fbd9f6272594f996f65240d8d2711e0
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-26 16:41:02 +0200

    v$open_cursor

---
+++ Installed/dynamic-performance-views/open_cursor/own-sessions-open-cursors.sql
@@ -0,0 +1,18 @@
+select
+  opc.user_name,
+--mon.username,
+--mon.module,
+  opc.sql_id,
+  opc.sql_exec_id,
+--mon.status,
+  opc.sql_text,
+  opc.last_sql_active_time,
+  opc.cursor_type
+from
+  v$session        ses                                       join
+  v$open_cursor    opc on ses.sid          = opc.sid /* left join
+  v$sql_monitor    mon on opc.sql_exec_id  = mon.key */
+where
+  ses.audsid = sys_context('userenv', 'sessionid')
+order by
+  decode (user_name, 'SYS', 1, 0);
