755979d2da2a9973a4a4c561ad2ef3b11f56b5be
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-26 06:44:24 +0200

    Left join to v$bgprocess

--- Installed/dynamic-performance-views/process/joins.sql
+++ Installed/dynamic-performance-views/process/joins.sql
@@ -2,7 +2,9 @@ select
   prc.pid,
   prc.serial#,
   prc.program,
-  pxp.status    px_status
+  bgp.description bg_process,
+  pxp.status      px_status
 from
-  v$process     prc                       left join
-  v$px_process  pxp on prc.pid = pxp.pid;
+  v$process     prc                        left join
+  v$px_process  pxp on prc.pid  = pxp.pid  left join
+  v$bgprocess   bgp on prc.addr = bgp.paddr; 
