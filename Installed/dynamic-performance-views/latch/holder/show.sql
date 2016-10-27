5616214c157e8dec9f3ec8007ae26c23e57142d8
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-27 08:36:02 +0200

    v$latchholder: show.sql

---
+++ Installed/dynamic-performance-views/latch/holder/show.sql
@@ -0,0 +1,14 @@
+select
+   lah.laddr             latch_address,
+   lah.name              latch_name,
+   prc.latchwait,
+   prc.latchspin,
+   prc.pname,
+   prc.username          prc_username,
+   ses.username          ses_username,
+   ses.osuser,
+   lah.gets
+from
+  v$latchholder lah                         join
+  v$session     ses on lah.sid = ses.sid    join
+  v$process     prc on lah.pid = prc.pid;
