dee4caceedebdc2109820feddedbb62db0d9c42c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-31 09:53:09 +0100

    two possibilities to find own session

--- Installed/dynamic-performance-views/session/identify-own-session.sql
+++ Installed/dynamic-performance-views/session/identify-own-session.sql
@@ -1,2 +1,4 @@
--- The »own« session can be found like so:
-select * from v$session where audsid = userenv('sessionid');
+-- The »own« session can be found with sys_context with two possibilities:
+
+select * from v$session where sid    = sys_context('userenv', 'sid'      );
+select * from v$session where audsid = sys_context('userenv', 'sessionid');
