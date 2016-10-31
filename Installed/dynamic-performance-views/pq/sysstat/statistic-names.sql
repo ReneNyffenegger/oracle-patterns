80d70d5b5447aa0604bf5b81cc6263b23b896f07
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-10-31 07:44:39 +0100

    v$pq_sysstat: statistic-names.sql

---
+++ Installed/dynamic-performance-views/pq/sysstat/statistic-names.sql
@@ -0,0 +1,27 @@
+select
+  statistic
+from
+  v$pq_sysstat
+order by
+  lower(statistic);
+--
+-- DDL Initiated
+-- DDL Initiated (IPQ)
+-- DFO Trees
+-- Distr Msgs Recv'd
+-- Distr Msgs Sent
+-- DML Initiated
+-- DML Initiated (IPQ)
+-- Local Msgs Recv'd
+-- Local Msgs Sent
+-- Queries Initiated
+-- Queries Initiated (IPQ)
+-- Queries Queued
+-- Server Sessions
+-- Servers Busy
+-- Servers Cleaned Up
+-- Servers Highwater
+-- Servers Idle
+-- Servers Shutdown
+-- Servers Started
+-- Sessions Active
