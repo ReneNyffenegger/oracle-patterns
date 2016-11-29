b7842b7a7a35f9163e5b172e5975aa4ef30bd672
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-11-29 09:08:23 +0100

    Four combinations: {-,desc}, {nulls last/first}

--- SQL/select/order_by/test_01.sql
+++ SQL/select/order_by/test_01.sql
@@ -3,17 +3,46 @@ create table tq84_order_by (
   value number
 );
 
-insert into tq84_order_by values ('seven',    7);
-insert into tq84_order_by values ('nine' ,    9);
-insert into tq84_order_by values ('five' ,    5);
-insert into tq84_order_by values ('eight',    8);
 insert into tq84_order_by values ('two'  ,    2);
-insert into tq84_order_by values ('foo'  , null);
+insert into tq84_order_by values ('null' , null);
 insert into tq84_order_by values ('four' ,    4);
 insert into tq84_order_by values ('three',    3);
-insert into tq84_order_by values ('six'  ,    6);
 insert into tq84_order_by values ('one'  ,    1);
 
-select * from tq84_order_by order by value desc nulls last;
+select * from tq84_order_by order by value;
+-- TXT             VALUE
+-- ---------- ----------
+-- one                 1
+-- two                 2
+-- three               3
+-- four                4
+-- null
 
-drop table tq84_order_by;
+select * from tq84_order_by order by value nulls first;
+-- TXT             VALUE
+-- ---------- ----------
+-- null
+-- one                 1
+-- two                 2
+-- three               3
+-- four                4
+
+select * from tq84_order_by order by value desc;
+-- TXT             VALUE
+-- ---------- ----------
+-- null
+-- four                4
+-- three               3
+-- two                 2
+-- one                 1
+
+select * from tq84_order_by order by value desc nulls last;
+-- TXT             VALUE
+-- ---------- ----------
+-- four                4
+-- three               3
+-- two                 2
+-- one                 1
+-- null
+
+drop table tq84_order_by purge;
