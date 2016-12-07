a994b9888c0f8d0f7d4870e73e4a9e382ec1c120
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-07 10:21:51 +0100

    mulitset except

---
+++ SQL/functions/multiset/except.sql
@@ -0,0 +1,48 @@
+create type tq84_obj as object (
+  a number,
+  b varchar2(10)
+);
+/
+
+create type tq84_obj_t as table of tq84_obj;
+/
+
+create table tq84_tab (
+  id  varchar2(10),
+  val tq84_obj_t
+)
+nested table val store as tq84_tab_val;
+
+insert into tq84_tab values ('1-3', tq84_obj_t(tq84_obj(1, 'one' ), tq84_obj(2, 'two'  ), tq84_obj(3, 'three')                      ));
+insert into tq84_tab values ('2-5', tq84_obj_t(tq84_obj(2, 'two' ), tq84_obj(3, 'three'), tq84_obj(4, 'four' ), tq84_obj(5, 'five' )));
+insert into tq84_tab values ('4'  , tq84_obj_t(tq84_obj(4, 'four')                                                                  ));
+insert into tq84_tab values ('n/a', tq84_obj_t(                                                                                     ));
+
+
+break on what skip 1
+
+select
+  a.id || ' except ' || b.id what,
+  t.*
+from
+  tq84_tab a,
+  tq84_tab b,
+  table(a.val multiset except b.val) t
+where
+  a.id = '1-3' and b.id = '2-5' or
+  a.id = '2-5' and b.id = '4'   or
+  a.id = '4'   and b.id = 'n/a';
+-- 
+-- WHAT                                  A B
+-- ---------------------------- ---------- ----------
+-- 1-3 except 2-5                        1 one
+-- 
+-- 2-5 except 4                          2 two
+--                                       3 three
+--                                       5 five
+-- 
+-- 4 except n/a                          4 four
+
+drop table tq84_tab;
+drop type  tq84_obj_t;
+drop type  tq84_obj;
