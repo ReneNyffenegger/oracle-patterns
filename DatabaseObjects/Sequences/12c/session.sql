2e4722c514e40569183dca24632c590455c927de
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-08 12:45:07 +0100

    alter sequence .. global|session

--- DatabaseObjects/Sequences/12c/session.sql
+++ DatabaseObjects/Sequences/12c/session.sql
@@ -15,3 +15,8 @@ select session_seq.nextval from dual;
 select session_seq.nextval from dual;
 
 drop sequence session_seq;
+
+--
+-- Note: a session level sequence can be changed to a global sequence and vice versa
+-- alter sequence session_seq global;
+-- alter sequence session_seq session;
