2fc924be1e8cef1dd42b03cba5425a4c6b92f162
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-08 13:46:04 +0100

    Sequence as defalt value for column

---
+++ DatabaseObjects/Sequences/default-value-for-column.sql
@@ -0,0 +1,22 @@
+create sequence tq84_seq start with 1 increment by 1;
+
+create table tq84_tab (
+  id  number default tq84_seq.nextval primary key,
+  txt varchar2(10)
+);
+
+insert into tq84_tab (txt) values ('foo');
+insert into tq84_tab (txt) values ('bar');
+insert into tq84_tab (txt) values ('baz');
+
+select * from tq84_tab;
+-- 
+--         ID TXT
+-- ---------- ----------
+--          1 foo
+--          2 bar
+--          3 baz
+
+
+drop table    tq84_tab;
+drop sequence tq84_seq;
