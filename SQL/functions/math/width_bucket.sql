ff7e2bcb00ac8427f355885f39830c2d1a7102f6
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-08 15:28:54 +0100

    show statements' result

--- SQL/functions/math/width_bucket.sql
+++ SQL/functions/math/width_bucket.sql
@@ -35,6 +35,17 @@ from (
 group by bucket_no
 order by bucket_no
 ;
+-- 
+-- BUCKET       S
+-- ---------- ---
+-- 7-13         3
+-- 14-20        2
+-- 21-27        7
+-- 28-34        5
+-- 35-41        6
+-- 42-48        3
+-- 49-55        3
+-- 56-62        2
 
 
 column t format a10
@@ -42,7 +53,7 @@ column t format a10
 select
   ( bucket_no    * 7    ) || '-' ||
   ((bucket_no+1) * 7 -1 )            bucket,
-   lpad('#', sum(1), '#')    t
+   lpad('o', sum(1), 'o')    t
 from (
   select
     width_bucket(v, 0, 56, 8)  bucket_no
@@ -52,5 +63,16 @@ from (
 group by bucket_no
 order by bucket_no
 ;
+-- 
+-- BUCKET     T
+-- ---------- ----------
+-- 7-13       ooo
+-- 14-20      oo
+-- 21-27      ooooooo
+-- 28-34      ooooo
+-- 35-41      oooooo
+-- 42-48      ooo
+-- 49-55      ooo
+-- 56-62      oo
 
 drop table tq84_hist purge;
