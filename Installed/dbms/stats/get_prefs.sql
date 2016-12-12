51d6d1aa39f19aae0bd5c89f4890dafcfb29991e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-12 11:48:48 +0100

    add 12.c option values

--- Installed/dbms/stats/get_prefs.sql
+++ Installed/dbms/stats/get_prefs.sql
@@ -12,10 +12,10 @@ from (
   select 'granularity'                 name from dual union all
   select 'publish'                     name from dual union all
   select 'incremental'                 name from dual union all
---select 'incremental_staleness'       name from dual union all
---select 'incremental_level'           name from dual union all
-  select 'stale_percent'               name from dual
---select 'global_temp_table_stats'     name from dual union all
+  select 'incremental_staleness'       name from dual union all
+  select 'incremental_level'           name from dual union all
+  select 'stale_percent'               name from dual union all
+  select 'global_temp_table_stats'     name from dual union all
 --select 'table_cached_blockes'        name from dual union all
---select 'options'                     name from dual
+  select 'options'                     name from dual
 ) p;
