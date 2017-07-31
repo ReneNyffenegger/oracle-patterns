5a5d1087307b77613429bf40eb13cad8db14a883
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-31 13:00:54 +0200

    Use inline clause

--- DatabaseObjects/Tables/ILM/add-policy.sql
+++ DatabaseObjects/Tables/ILM/add-policy.sql
@@ -3,7 +3,10 @@ create table tq84_ilm_policy (
   col_1 varchar2(10),
   col_2 varchar2(10)
 )
+  ilm add policy row store compress advanced segment after 3 days of no modification
 ;
 
-alter table tq84_ilm_policy 
-  ilm add policy row store compress advanced segment after 3 days of no modification;
+-- Alternatively, with alter table:
+--
+--    alter table tq84_ilm_policy 
+--       ilm add policy row store compress advanced segment after 3 days of no modification;
