df34f292fac235d541cda6e60dd7718abdf428e9
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-08-03 08:32:27 +0200

    Create select.sql

---
+++ datatypes/rowid/select.sql
@@ -0,0 +1,16 @@
+create table tq84_rowid (
+  id    number,
+  col_1 varchar2(20)
+);
+
+insert into tq84_rowid values(1, 'foo');
+insert into tq84_rowid values(2, 'bar');
+insert into tq84_rowid values(3, 'baz');
+
+select
+  rowid
+  r.*
+from
+  tq84_rowid r;
+
+drop table tq84_rowid;
