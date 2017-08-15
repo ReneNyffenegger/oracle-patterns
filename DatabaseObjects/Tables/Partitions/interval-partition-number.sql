3bd29b4eb14a20725907f23a05a43c682e32c12f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-08-15 15:52:09 +0200

    Update interval-partition-number.sql

--- DatabaseObjects/Tables/Partitions/interval-partition-number.sql
+++ DatabaseObjects/Tables/Partitions/interval-partition-number.sql
@@ -4,21 +4,35 @@ create table tq84_interval_partition_number (
   dt   date
 )
 partition by range (id) 
-interval (1) (
-  partition tq84_int_part_num_1 values less than (1)  
+interval (1000) (
+  partition tq84_int_part_num_1 values less than (0)  
 );
 
 insert into tq84_interval_partition_number values (0, 'foo', null   );
 insert into tq84_interval_partition_number values (1, 'bar', sysdate);
 insert into tq84_interval_partition_number values (2, 'baz', null   );
 
+insert into tq84_interval_partition_number values (1000, '*', null   );
+
+insert into tq84_interval_partition_number values (99500, 'a', null   );
+insert into tq84_interval_partition_number values (99501, 'b', null   );
+insert into tq84_interval_partition_number values (99502, 'c', null   );
+insert into tq84_interval_partition_number values (99503, 'd', null   );
+
+exec dbms_stats.gather_table_stats(user, 'tq84_interval_partition_number');
+
 select
-  partition_name
+  partition_name,
+  num_rows,
+  high_value,
+  interval,
+  segment_created
 from
   user_tab_partitions
 where
-  table_name = 'TQ84_INTERVAL_PARTITION_NUMBER';
-
+  table_name = 'TQ84_INTERVAL_PARTITION_NUMBER'
+;
+  
 explain plan for select * from tq84_interval_partition_number where id = 2;
 select * from table(dbms_xplan.display);
 
