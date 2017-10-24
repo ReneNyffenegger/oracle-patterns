10021471edf547cf45679ffb73b4684ffefb78d3
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-10-24 10:03:22 +0200

    Create composite-interval-list.sql

---
+++ DatabaseObjects/Tables/Partitions/composite-interval-list.sql
@@ -0,0 +1,28 @@
+
+create table tq84_part_interval_list (
+  id   number,
+  txt  varchar2(10),
+  cat  varchar2( 3),
+  dt   date
+)
+partition by range (dt) interval (numtoyminterval(1, 'month'))
+    subpartition by list (cat)
+        subpartition template (
+          subpartition foo values ('foo'),
+          subpartition bar values ('bar'),
+          subpartition baz values ('baz')
+        )
+(
+  partition values less than ( date '2017-01-01') 
+);
+
+insert into tq84_part_interval_list values (1, 'abc', 'foo', date '2017-03-01');
+insert into tq84_part_interval_list values (2, 'def', 'bar', date '2017-03-02');
+insert into tq84_part_interval_list values (3, 'ghi', 'bar', date '2017-03-03');
+insert into tq84_part_interval_list values (4, 'jkl', 'foo', date '2017-12-01');
+insert into tq84_part_interval_list values (5, 'mno', 'bar', date '2017-12-02');
+insert into tq84_part_interval_list values (6, 'pqr', 'bar', date '2017-12-03');
+
+select * from user_tab_subpartitions where table_name = 'TQ84_PART_INTERVAL_LIST';
+
+drop table tq84_part_interval_list purge;
