8c9a401de7ba7b71b01faa8179e7f50a2f6f9785
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-09-03 12:36:16 +0200

    + user_all_dba_cdb_view.sql

--- Installed/data-dictionary/user_all_dba_cdb_view.sql
+++ Installed/data-dictionary/user_all_dba_cdb_view.sql
@@ -1,8 +1,4 @@
---
--- 2019-09-03: use user_all_dba_cdb_view.sql
---
-column obj format a30
-
+create view user_all_dba_cdb_v as
 with u as ( select substr(object_name, 6) obj, object_name from dba_objects where owner = 'SYS' and object_name like 'USER\_%' escape '\'),
      a as ( select substr(object_name, 5) obj, object_name from dba_objects where owner = 'SYS' and object_name like  'ALL\_%' escape '\'),
      d as ( select substr(object_name, 5) obj, object_name from dba_objects where owner = 'SYS' and object_name like  'DBA\_%' escape '\'),
@@ -12,16 +8,15 @@ with u as ( select substr(object_name, 6) obj, object_name from dba_objects wher
             select obj from d union
             select obj from c)
 select
-  nvl2(u.obj, 'USER', '-'),
-  nvl2(a.obj, 'ALL' , '-'),
-  nvl2(d.obj, 'DBA' , '-'),
-  nvl2(d.obj, 'CDB' , '-'),
-       o.obj 
+  nvl2(u.obj, 'USER', '-') u,
+  nvl2(a.obj, 'ALL' , '-') a,
+  nvl2(d.obj, 'DBA' , '-') d,
+  nvl2(d.obj, 'CDB' , '-') c,
+       o.obj               o
 from
   o                  left join
   u on o.obj = u.obj left join
   a on o.obj = a.obj left join
-  d on o.obj = d.obj left join
-  c on o.obj = c.obj
+  d on o.obj = d.obj
 order by
   o.obj;
