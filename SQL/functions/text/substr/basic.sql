a7790b5116ccd9c361829db7b330835e7618ce18
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-09-08 07:59:50 +0200

    + two-parameters.sql

--- SQL/functions/text/substr/basic.sql
+++ SQL/functions/text/substr/basic.sql
@@ -1,17 +1,6 @@
 select
    substr(
       '1234567890', -- Text
-       5            -- Start position
-   )
-from
-   dual;
---
--- 567890
-
-
-select
-   substr(
-      '1234567890', -- Text
        5          , -- Start position
        3            -- Length
    )
---
+++ SQL/functions/text/substr/two-parameters.sql
@@ -0,0 +1,9 @@
+select
+   substr(
+      '1234567890', -- Text
+       5            -- Start position
+   )
+from
+   dual;
+--
+-- 567890
