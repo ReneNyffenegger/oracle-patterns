a2f83fcedae9363a7bd296cb29f18eeaec06e93a
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-10-29 13:26:33 +0100

    Join with previos sql as well

--- Installed/dynamic-performance-views/session/find-my-interesting-sessions.sql
+++ Installed/dynamic-performance-views/session/find-my-interesting-sessions.sql
@@ -1,16 +1,21 @@
 select /*+ ordered */
-  ses.sid,
-  ses.serial#,
-  ses.program,
-  ses.osuser,
-  ses.event,
-  round(ses.wait_time_micro/1000/1000, 2) wait_s,
-  sql.sql_text
+-- ses.sid,
+-- ses.saddr,
+-- ses.serial#,
+   ses.program,
+-- ses.osuser,
+   ses.event,
+   round(ses.wait_time_micro/1000/1000, 2) wait_s,   
+-- ses.sql_id,
+-- ses.prev_sql_id,
+   ses.logon_time,
+   substr(sql.sql_text, 1, 50)             cur_sql_text,
+   substr(sqp.sql_text, 1, 50)             prev_sql_text
 from
-  v$session  ses  left join
-  v$sqlarea  sql on ses.sql_id = sql.sql_id
+   v$session  ses                                    left join
+   v$sqlarea  sql on ses.sql_id      = sql.sql_id    left join
+   v$sqlarea  sqp on ses.prev_sql_id = sqp.sql_id
 where
---username = 'RNYFF'         and
-  osuser   = 'rnyffenegger'  and
+  osuser   = 'rene'          and
 --program  = 'SQL Developer' and
   program  = 'sqlplus.exe';
