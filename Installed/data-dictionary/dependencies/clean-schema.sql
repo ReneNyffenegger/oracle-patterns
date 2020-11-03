9f5dce5e2c932261f0b554c25df772166ba490e9
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-11-03 14:57:54 +0100

    + clean-schema.sql

---
+++ Installed/data-dictionary/dependencies/clean-schema.sql
@@ -0,0 +1,50 @@
+declare
+   objects_available boolean;
+begin
+
+   if user != 'RENE' then -- {
+      dbms_output.put_line('Script must be run as RENE');
+      return;
+   end if; -- }
+
+   if sys_context('userenv', 'db_name') != 'THEDB' then -- {
+      dbms_output.put_line('Script must be run on database THEDB');
+      return;
+   end if; -- }
+
+   loop -- {
+      objects_available := false;
+
+      for r in ( -- {
+        select
+            obj.object_name,
+            obj.object_type
+         from
+            user_objects     obj
+         where
+            obj.object_type not in ('INDEX', 'LOB', 'PACKAGE_BODY')
+       minus
+         select
+            referenced_name       ,
+            referenced_type
+        from
+            user_dependencies
+        where
+         not
+         (
+            (referenced_type = 'PACKAGE'      and referenced_owner = 'SYS' and referenced_name = 'STANDARD') or
+            (referenced_type = 'PACKAGE BODY' and referenced_owner = 'SYS' and referenced_name = 'STANDARD')
+         )
+      )
+      loop
+          dbms_output.put_line('drop ' || r.object_type || ' ' || r.object_name || case when r.object_type = 'TABLE' then ' purge' end);
+          objects_available := true;
+          execute immediate    'drop ' || r.object_type || ' ' || r.object_name || case when r.object_type = 'TABLE' then ' purge' end ;
+      end loop; -- }
+
+      exit when not objects_available;
+
+   end loop; -- }
+
+end;
+/
