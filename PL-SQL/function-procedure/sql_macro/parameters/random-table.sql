3776376d85ff8b0f5cd0383179891ebbcf84c5ae
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-11-07 21:40:25 +0100

    Add compile and exec time

--- PL-SQL/function-procedure/sql_macro/parameters/random-table.sql
+++ PL-SQL/function-procedure/sql_macro/parameters/random-table.sql
@@ -1,5 +1,15 @@
 create or replace function randomTable return varchar2 sql_macro
-is begin
-   return 'select * from T' || mod(trunc(extract(second from systimestamp)), 2);
+is
+   compile_date varchar2(19) := to_char(sysdate, 'yyyy-mm-dd hh24:mi:ss');
+begin
+
+   return '
+      select
+      ''' || compile_date || '''                     compile_date,
+         to_char(sysdate, ''yyyy-mm-dd hh24:mi:ss'') exec_date,
+         txt
+      from
+         T' || mod(trunc(extract(second from systimestamp)), 2);
+
 end randomTable;
 /
