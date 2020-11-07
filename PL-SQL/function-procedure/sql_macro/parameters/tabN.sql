975f97925b18b9a4fd2e30f6d1a97f2a36dfd102
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-11-07 22:17:38 +0100

    add some spacing

--- PL-SQL/function-procedure/sql_macro/parameters/tabN.sql
+++ PL-SQL/function-procedure/sql_macro/parameters/tabN.sql
@@ -1,9 +1,10 @@
---drop function tabN;
-
 create or replace function tabN(n integer) return varchar2 sql_macro
-is begin
+is
+begin
+
    dbms_output.put_line('tabN being called, n = ' || n);
+
    return 'select * from T' || mod(n, 2);
+
 end tabN;
 /
-
