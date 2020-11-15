947950c6b307dd021a44d685e6504a5cbf33ee8c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-11-15 19:47:27 +0100

    show seconds ago, filter with user

--- Installed/dynamic-performance-views/sql/area/parsed-by-user.sql
+++ Installed/dynamic-performance-views/sql/area/parsed-by-user.sql
@@ -1,14 +1,18 @@
 select
--- sql.last_load_time,
-   sql.last_active_time,
+-- sql.last_active_time,
+   round((sysdate - sql.last_active_time) * 24*60*60) last_active_secs_ago,
 -- sql.first_load_time,
    sql.sql_id,
-   sql.sql_text
--- sql.*
+   sql.sql_text,
+   sql.executions,
+   sql.parse_calls,
+   sql.last_load_time,
+   sql.*
 from
    v$sqlarea   sql                                        join
    dba_users   usr on sql.parsing_user_id = usr.user_id
 where
-   usr.username = 'RENE'
+   usr.username = user
 order by
-   sql.last_active_time desc;
+   sql.last_active_time desc
+;
