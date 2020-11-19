21bf0a100e072696e6ec81b9416a15ef793d6bb0
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-11-19 22:39:58 +0100

    fix seconds ago

--- PL-SQL/pragma/autonomous_transaction/logger/view.sql
+++ PL-SQL/pragma/autonomous_transaction/logger/view.sql
@@ -1,7 +1,10 @@
 create or replace view tq84_log_view as
 select
    txt,
-   extract(second from (systimestamp - ts)) sec_ago,
+   extract(day    from (systimestamp - ts)) days_ago   ,
+   extract(hour   from (systimestamp - ts)) hours_ago  ,
+   extract(minute from (systimestamp - ts)) minutes_ago,
+   extract(second from (systimestamp - ts)) seconcs_ago,
    id
 from
    tq84_log_table
