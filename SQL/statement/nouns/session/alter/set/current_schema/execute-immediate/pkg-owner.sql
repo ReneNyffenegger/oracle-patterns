b557515c33306d29ac1a5e40a3545280241c2de7
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-12-16 11:20:04 +0100

    + add pkg with authid current_user

--- SQL/statement/nouns/session/alter/set/current_schema/execute-immediate/pkg-owner.sql
+++ SQL/statement/nouns/session/alter/set/current_schema/execute-immediate/pkg-owner.sql
@@ -1,5 +1,10 @@
-create package PKG_DEFINER
-   authid definer
+create package PKG_DEFINER AUTHID DEFINER
+as
+   procedure do_something;
+end;
+/
+
+create package PKG_CURRENT_USER AUTHID CURRENT_USER
 as
    procedure do_something;
 end;
@@ -10,11 +15,13 @@ as
    procedure do_something as
    begin
 
+      dbms_output.put_line('pkg_definer');
+
       execute immediate q'{
           begin
-             dbms_output.put_line('exec immediate: current schema before setting current_schema: ' || sys_context('userenv', 'current_schema'));
+             dbms_output.put_line('  exec immediate: current schema before setting current_schema: ' || sys_context('userenv', 'current_schema'));
              execute immediate 'alter session set current_schema = pkg_user';
-             dbms_output.put_line('exec immediate: current schema after  setting current_schema: ' || sys_context('userenv', 'current_schema'));
+             dbms_output.put_line('  exec immediate: current schema after  setting current_schema: ' || sys_context('userenv', 'current_schema'));
           end;
       }';
 
@@ -24,11 +31,40 @@ as
    -- executed in the execute statement. It does not change
    -- the enclosing scope.
    --
-      dbms_output.put_line('current schema after execute immediate:                       ' || sys_context('userenv', 'current_schema'));
+      dbms_output.put_line('  current schema after execute immediate:                       ' || sys_context('userenv', 'current_schema'));
 
    end do_something;
 end pkg_definer;
 /
 
-show errors
-grant execute on pkg_definer to pkg_user;
+create package body PKG_CURRENT_USER
+as
+   procedure do_something as
+   begin
+
+      
+      dbms_output.put_line('pkg_current_user');
+
+      execute immediate q'{
+          begin
+             dbms_output.put_line('  exec immediate: current schema before setting current_schema: ' || sys_context('userenv', 'current_schema'));
+             execute immediate 'alter session set current_schema = pkg_owner';
+             dbms_output.put_line('  exec immediate: current schema after  setting current_schema: ' || sys_context('userenv', 'current_schema'));
+          end;
+      }';
+
+   --
+   -- Turns out, the alter session set current_schema in the
+   -- previous execute immediate affects affects also the scope
+   -- of the enclosing  scope
+   --
+      dbms_output.put_line('  current schema after execute immediate:                       ' || sys_context('userenv', 'current_schema'));
+
+   end do_something;
+end pkg_current_user;
+/
+
+
+
+grant execute on pkg_definer      to pkg_user;
+grant execute on pkg_current_user to pkg_user;
--- SQL/statement/nouns/session/alter/set/current_schema/execute-immediate/pkg-user.sql
+++ SQL/statement/nouns/session/alter/set/current_schema/execute-immediate/pkg-user.sql
@@ -1,4 +1,5 @@
 begin
-   pkg_owner.pkg_definer.do_something;
+   pkg_owner.pkg_definer     .do_something;
+   pkg_owner.pkg_current_user.do_something;
 end;
 /
