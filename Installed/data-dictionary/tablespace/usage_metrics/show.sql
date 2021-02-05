34add778fc0a6c483ebfde6389695c92bf8edb7e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-02-05 20:25:36 +0100

    add more columns

--- Installed/data-dictionary/tablespace/usage_metrics/show.sql
+++ Installed/data-dictionary/tablespace/usage_metrics/show.sql
@@ -1,9 +1,15 @@
 select
-  tablespace_name,
-  used_space,
-  tablespace_size,
-  to_char(used_percent, '990.00') used_percent
+   tsm.tablespace_name,
+   round(tsm.used_space       / 1024/1024,1)  used_space_mb,
+   round(ts.max_size          / 1024/1024,1)  max_size_mb,
+   round(tsm.tablespace_size  / 1024/1024,1)  size_mb,
+   to_char(tsm.used_percent, '990.00')        used_percent,
+   ts.block_size              / 1024          block_size_kb,
+   ts.logging,
+   ts.def_tab_compression,
+   ts.compress_for
 from
-  dba_tablespace_usage_metrics m
+   dba_tablespace_usage_metrics tsm  join
+   dba_tablespaces              ts on tsm.tablespace_name = ts.tablespace_name
 order by
-  m.used_percent desc;
+   tsm.used_percent desc;
