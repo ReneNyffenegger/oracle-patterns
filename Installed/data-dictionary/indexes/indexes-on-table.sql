a2aea7ae0f417a4f7c9db0ff22e9e48ccd7fada0
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-03-12 11:28:04 +0100

    Indexes on table

--- Installed/data-dictionary/indexes/indexes-on-table.sql
+++ Installed/data-dictionary/indexes/indexes-on-table.sql
@@ -1,17 +1,21 @@
 select
 -- ic.table_name,
-   case when ic.column_position = 1 then ix.index_name else '' end index_name,
-   ic.column_name,
-   ix.index_type,
-   ix.table_type,
+   case when ic.column_position = 1 then ix.index_name         end index_name,
+             ic.column_name,
+   case when ic.column_position = 1 then ix.index_type         end index_type,
+   case when ic.column_position = 1 then ix.table_type         end table_type,
+   case when ic.column_position = 1 then iu.last_used          end last_used,
+   case when ic.column_position = 1 then iu.total_access_count end total_access_count,
    ic.column_position
 from
-   dba_indexes      ix  join
+   dba_indexes      ix                                               join
    dba_ind_columns  ic on ix.owner      = ic.index_owner and
-                          ix.index_name = ic.index_name
+                          ix.index_name = ic.index_name         left join
+   dba_index_usage  iu on ix.owner      = iu.owner       and
+                          ix.index_name = iu.name
 where
-   ix.owner      = 'K' and
-   ix.table_name = 'BALSTRU_SERPIL'
+   ix.owner      = '...' and
+   ix.table_name = '...'
 order by
    ix.index_name,
    ic.column_position;
