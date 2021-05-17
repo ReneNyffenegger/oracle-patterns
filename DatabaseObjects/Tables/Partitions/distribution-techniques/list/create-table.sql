4eda80215178b6f463356e21a518287522631af0
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-05-17 12:50:13 +0200

    list partitioned table

--- DatabaseObjects/Tables/Partitions/distribution-techniques/list/create-table.sql
+++ DatabaseObjects/Tables/Partitions/distribution-techniques/list/create-table.sql
@@ -1,3 +1,5 @@
+drop table tq84_list_partition purge;
+
 create table tq84_list_partition (
   id     number,
   txt    varchar2(10),
@@ -13,10 +15,11 @@ partition by list (sector) (
 );
 
 select partition_name from user_tab_partitions where table_name = 'TQ84_LIST_PARTITION';
-
-
-explain plan for select * from tq84_list_partition_south where sector = 'East';
-select * from table(dbms_xplan.display);
+--
+-- TQ84_LIST_PARTITION_EAST
+-- TQ84_LIST_PARTITION_NORTH
+-- TQ84_LIST_PARTITION_SOUTH
+-- TQ84_LIST_PARTITION_WEST
 
 insert into tq84_list_partition values (1, 'eee', 'East');
 insert into tq84_list_partition values (2, 'EEE', 'East');
@@ -25,6 +28,3 @@ insert into tq84_list_partition values (3, 'w'  , 'West');
 insert into tq84_list_partition values (4, 'ww' , 'West');
 
 commit;
-
-
-drop table tq84_list_partition purge;
