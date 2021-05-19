6c4c77ca1b2cef16c0403a8267ea8392d04f7d10
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-05-19 14:29:59 +0200

    add specific statistics

--- Installed/dynamic-performance-views/sesstat/join-session.sql
+++ Installed/dynamic-performance-views/sesstat/join-session.sql
@@ -1,19 +1,34 @@
 select
    sess.sid,
    sess.serial#,
--- sess.username,
+   sess.username,
+   sess.osuser,
 -- sess.osuser,
 -- sess.program,
 -- sest.sid,
    stat.name,
-   sest.value
+   sest.value,
+   sess.module,
+   sess.status
 from
    v$session  sess                                         join
    v$sesstat  sest on sess.sid = sest.sid                  join
    v$statname stat on sest.statistic# = stat.statistic#
 where
-   sess.osuser  = 'rene'       and
-   sess.program = 'EXCEL.EXE'  and
-   sest.value   >  0
+   sess.type   != 'BACKGROUND' and
+-- sess.osuser  = 'rene'       and
+-- sess.program = 'EXCEL.EXE'  and
+   sest.value   >  0           and
+   sess.sid     = 574          and
+   stat.name in (
+     'recursive calls',
+     'db block gets'  ,
+     'consistent gets',
+     'physical reads' ,
+     'redo size'      ,
+     'sorts (memory)' ,
+     'sorts (disk)'   ,
+     'rows processed'
+   )
 order by
    lower(name);
