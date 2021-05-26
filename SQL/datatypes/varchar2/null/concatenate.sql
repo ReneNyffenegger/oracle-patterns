0bc7b073b1238b37926255f376d0850e7fa1abc3
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-05-26 10:49:15 +0200

    + concatenate.sql

---
+++ SQL/datatypes/varchar2/null/concatenate.sql
@@ -0,0 +1,10 @@
+select
+   text_1 || text_2 concatenated
+from
+   tq84_null_test;
+--
+-- CONCATENATED
+-- ----------------------------------------
+-- helloworld
+-- other is null
+-- other is empty
