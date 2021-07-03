20197d4d5706ee6c82bb483899522bc2db68b135
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-07-03 17:51:44 +0200

    rework example

--- Installed/dbms/snapshot/refresh_commit.sql
+++ Installed/dbms/snapshot/refresh_commit.sql
@@ -3,7 +3,7 @@ create table tq84_r1 (
    b   number
 );
 
-insert into tq84_r1 values ('foo',   1);
+insert into tq84_rl values ('foo',   1);
 insert into tq84_r1 values ('foo',   2);
 insert into tq84_r1 values ('foo',   3);
 
@@ -13,46 +13,37 @@ insert into tq84_r1 values ('bar',  50);
 insert into tq84_r1 values ('baz', 100);
 
 create materialized view tq84_mv
-  refresh force on demand
+   refresh force on demand
 as
-  select
-    a,
-    sum(b) sum_b
+   select
+      a,
+      sum(b) sum_b
   from
-    tq84_r1
+     tq84_r1
   group by
-    a;
-
-
+     a;
 
 insert into tq84_r1 values ('foo',  4);
 insert into tq84_r1 values ('bar', 10);
 
 select * from tq84_mv;
 
-
 create procedure refresh_ as
     dummy number;
 begin
-
     dbms_snapshot.refresh('tq84_mv');
 
 --  Refresh stays in effect, although next line
 --  throws error:
-
     dummy := 42/0;
-
 end refresh_;
 /
 
 exec refresh_;
-
 select * from tq84_mv;
-
+rollback;
 select * from tq84_mv;
 
 drop procedure refresh_;
-
 drop materialized view tq84_mv;
-
 drop table tq84_r1;
