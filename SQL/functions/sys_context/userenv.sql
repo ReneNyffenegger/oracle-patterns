b3a4a01bb0f8ca21949b613629a035b9bbc8c76d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-09-21 14:14:41 +0200

    uncomment some parameters

--- SQL/functions/sys_context/userenv.sql
+++ SQL/functions/sys_context/userenv.sql
@@ -5,12 +5,12 @@ with name as (
    select 'authentication_data'        space from dual union all
    select 'authentication_method'      space from dual union all
    select 'bg_job_id'                  space from dual union all
--- select 'cdb_name'                   space from dual union all
+   select 'cdb_name'                   space from dual union all
    select 'client_identifier'          space from dual union all
    select 'client_info'                space from dual union all
--- select 'client_program_name'        space from dual union all
--- select 'con_id'                     space from dual union all
--- select 'con_name'                   space from dual union all
+   select 'client_program_name'        space from dual union all
+   select 'con_id'                     space from dual union all
+   select 'con_name'                   space from dual union all
 -- select 'current_bind'               space from dual union all  -- Only within event handlers for fine-grained auditing
    select 'current_edition_name'       space from dual union all
    select 'current_schema'             space from dual union all
@@ -22,7 +22,7 @@ with name as (
    select 'database_role'              space from dual union all
    select 'db_domain'                  space from dual union all
    select 'db_name'                    space from dual union all
--- select 'db_supplemental_log_level'  space from dual union all
+   select 'db_supplemental_log_level'  space from dual union all
    select 'db_unique_name'             space from dual union all  -- Corresponds to init param with same name
    select 'dblink_info'                space from dual union all
    select 'entryid'                    space from dual union all
@@ -36,7 +36,7 @@ with name as (
    select 'instance_name'              space from dual union all
    select 'ip_address'                 space from dual union all
 -- select 'is_apply_server'            space from dual union all
--- select 'is_dg_rolling_upgrade'      space from dual union all
+   select 'is_dg_rolling_upgrade'      space from dual union all
    select 'isdba'                      space from dual union all
    select 'lang'                       space from dual union all
    select 'language'                   space from dual union all
@@ -47,9 +47,9 @@ with name as (
    select 'nls_date_language'          space from dual union all
    select 'nls_sort'                   space from dual union all
    select 'nls_territory'              space from dual union all
--- select 'oracle_home'                space from dual union all
+   select 'oracle_home'                space from dual union all
    select 'os_user'                    space from dual union all
--- select 'platform_slash'             space from dual union all
+   select 'platform_slash'             space from dual union all -- Slash type used to separate files and directories in path names.
    select 'policy_invoker'             space from dual union all
    select 'proxy_enterprise_identity'  space from dual union all
    select 'proxy_user'                 space from dual union all
@@ -66,7 +66,6 @@ with name as (
 -- select 'unified_session_id'         space from dual
 )
 select
-  name.space, substr(sys_context('userenv', name.space, 50), 1, 50)
+   name.space, substr(sys_context('userenv', name.space, 50), 1, 50)
 from
-  name;
-
+   name;
