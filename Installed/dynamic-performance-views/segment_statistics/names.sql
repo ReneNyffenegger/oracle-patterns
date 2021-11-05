f3ce894793ffa408def9c47385aca78a3575bc91
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-11-05 10:55:49 +0100

    compare with v$segstat_name

--- Installed/dynamic-performance-views/segment_statistics/names.sql
+++ Installed/dynamic-performance-views/segment_statistics/names.sql
@@ -1,24 +1,22 @@
-select 
-  distinct statistic_name
+select
+   distinct statistic_name
 from
-  v$segment_statistics
-order by
-  statistic_name;
--- buffer busy waits
--- db block changes
--- gc buffer busy
--- gc cr blocks received
--- gc current blocks received
--- ITL waits
--- logical reads
--- optimized physical reads
--- physical read requests
--- physical reads
--- physical reads direct
--- physical write requests
--- physical writes
--- physical writes direct
--- row lock waits
--- segment scans
--- space allocated
--- space used
+   v$segment_statistics
+                                    minus
+select
+   name
+from
+   v$segstat_name;
+
+---
+
+select
+   name
+from
+   v$segstat_name
+                                    minus
+select
+   distinct statistic_name
+from
+   v$segment_statistics
+;
