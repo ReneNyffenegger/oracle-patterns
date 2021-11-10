7357b652d2821c824507f950326bf71b2cb5b8bf
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-11-10 13:30:11 +0100

    + default-on-conversion-error.sql

---
+++ SQL/functions/type-conversion/to/number/default-on-conversion-error.sql
@@ -0,0 +1,7 @@
+select
+   to_number(  '22/3'  default -1 on conversion error) n1,
+   to_number(  '22.3'  default -1 on conversion error) n2,
+   to_number('  22.3'  default -1 on conversion error) n3,
+   to_number('  22.3x' default -1 on conversion error) n4
+from
+   dual;
