db6a6aeeb8af9ef3d8ed7251c6fe9c7c1485ea14
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-11-19 08:20:58 +0100

    add more columns, filter for unequal columns

--- DatabaseObjects/Tables/columns/diff-structure/create-tables.sql
+++ DatabaseObjects/Tables/columns/diff-structure/create-tables.sql
@@ -1,2 +1,5 @@
-create table x (a number, b number(2), c number(2,3), d varchar2(4)               );
-create table y (a number,              c number(2,3), b date       , e varchar2(5));
+drop   table y;
+drop   table x;
+
+create table x (a number, b number(2), c number(2,3), d varchar2(4)               , f date     null, g varchar2(20), h number(6,2));
+create table y (a number,              c number(2,3), d date       , e varchar2(5), f date not null, g varchar2(20), h number(7,2));
--- DatabaseObjects/Tables/columns/diff-structure/diff.sql
+++ DatabaseObjects/Tables/columns/diff-structure/diff.sql
@@ -4,17 +4,30 @@ select
      coalesce(a.column_name, b.column_name)                 col_name,
      --
      a.column_id        column_id_a     , b.column_id       column_id_b,
+  --
+  -- Columns with column information (data types, sizes etc.):
+  --
      a.data_type        data_type_a     , b.data_type       data_type_b     ,
      a.data_length      data_length_a   , b.data_length     data_length_b   ,
      a.data_scale       data_scale_a    , b.data_scale      data_scale_b    ,
      a.data_precision   data_precision_a, b.data_precision  data_precision_b,
      a.nullable         nullable_a      , b.nullable        nullable_b      ,
+  --
+  -- Columns with information not related to table structure:
+  --
      a.last_analyzed    last_analyzed_a , b.last_analyzed   last_analyzed_b ,
      a.sample_size      sample_size_a   , b.sample_size     sample_size_b   ,
      a.num_distinct     num_distinct_a  , b.num_distinct    num_distinct_b  ,
-     a.low_value        low_value_a     , b.low_value       low_value_b 
+     a.low_value        low_value_a     , b.low_value       low_value_b
 from
-     a                full outer join
+     a                                                full outer join
      b on a.column_name = b.column_name
+where
+  not (nvl(a.data_type      , 'x') = nvl(b.data_type     , 'x')  and
+       nvl(a.data_length    , -1 ) = nvl(b.data_length   , -1 )  and
+       nvl(a.data_scale     , -1 ) = nvl(b.data_scale    , -1 )  and
+       nvl(a.data_precision , -1 ) = nvl(b.data_precision, -1 )  and
+       nvl(a.nullable       , 'x') = nvl(b.nullable      , 'x')
+      )
 order by
     coalesce (a.column_id, b.column_id);
