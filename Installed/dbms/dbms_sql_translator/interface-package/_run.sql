3248113ae80003aac3645882921b7d503f44d398
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-01-12 12:56:24 +0100

    + dbms_sql_translator

---
+++ Installed/dbms/dbms_sql_translator/interface-package/_run.sql
@@ -0,0 +1,10 @@
+@ spec
+@ body
+@ profile
+@ set-profile
+
+-- alter session set events = '10601 trace name context forever, level 32';
+
+@ get-translated
+@ set-events
+@ select
---
+++ Installed/dbms/dbms_sql_translator/interface-package/set-profile.sql
@@ -0,0 +1,1 @@
+alter session set sql_translation_profile = renes_sql_translation_profile;
