e7290dce03c181fcb11750fb93124f690e8140b5
Rene Nyffenegger <mail@renenyffenegger.ch>
2022-01-25 09:18:22 +0100

    Update err-32611.sql

--- SQL/select/model/intro/err-32611.sql
+++ SQL/select/model/intro/err-32611.sql
@@ -4,7 +4,7 @@ from
    tq84_model_clause
 model
    dimension by ( row_ )
-   measures     (col_1, col_2, col_3, col_4, col_5 )
+   measures     ( col_1, col_2, col_3, col_4, col_5 )
    (
       col_3[cv() > 3] = 42
    )
