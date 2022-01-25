44057c5995714d54181acc3aaebd9d15d5c8db07
Rene Nyffenegger <mail@renenyffenegger.ch>
2022-01-25 09:19:05 +0100

    Update err-00984.sql

--- SQL/select/model/intro/err-00984.sql
+++ SQL/select/model/intro/err-00984.sql
@@ -4,9 +4,8 @@ from
    tq84_model_clause
 model
    dimension by ( row_ )
-   measures     (col_1, col_2, col_3, col_4, col_5 )
+   measures     ( col_1, col_2, col_3, col_4, col_5 )
    (
       col_1[row_] = col_1 * 2
    )
 ;
-
