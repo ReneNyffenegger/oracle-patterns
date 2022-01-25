9d1de8cac978f9560602a9f9baf6bab03335dd76
Rene Nyffenegger <mail@renenyffenegger.ch>
2022-01-25 09:17:21 +0100

    Update add-record.sql

--- SQL/select/model/intro/add-record.sql
+++ SQL/select/model/intro/add-record.sql
@@ -4,7 +4,7 @@ from
    tq84_model_clause
 model
    dimension by ( row_ )
-   measures     (col_1, col_2, col_3, col_4, col_5 )
+   measures     ( col_1, col_2, col_3, col_4, col_5 )
    (
       col_1[ 7 ] = 101,
       col_2[ 7 ] = 102,
