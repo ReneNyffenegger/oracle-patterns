89888335160122f2025ab62b4f212dc3a41dfa52
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-02-17 11:17:32 +0100

    join with dba_objects

--- Installed/dynamic-performance-views/active/session_history/join-session-sql.sql
+++ Installed/dynamic-performance-views/active/session_history/join-session-sql.sql
@@ -4,6 +4,8 @@ select
 -- ses.program,
    sql.sql_fulltext,
 -- ses.osuser
+   obj.object_name      obj_name,
+   obj.owner            obj_owner,
    ash.*,
    ash.sample_time_utc,
    ash.event,
@@ -13,11 +15,13 @@ select
    ash.sql_exec_id,
    to_char(ash.sql_exec_start, 'yyyy-mm-dd hh24:mi:ss') sql_exec_start
 from
-   v$session                ses                                                                    join
+   v$session                ses                                                                         join
    v$active_session_history ash on ses.sid = ash.session_id and ses.serial# = ash.session_serial#  left join
-   v$sqlarea                sql on ash.top_level_sql_id = sql.sql_id
-where
-   ses.osuser  = 'rene'            and
-   ses.program = 'sqldeveloper.exe'
+   v$sqlarea                sql on ash.top_level_sql_id = sql.sql_id                               left join
+   dba_objects              obj on ash.current_obj#     = obj.object_id
+--where
+-- ses.osuser  = 'RENE'            and
+-- ses.program = 'sqldeveloper.exe'
 order by
-   ash.sample_id;
+   ash.sample_id
+;
