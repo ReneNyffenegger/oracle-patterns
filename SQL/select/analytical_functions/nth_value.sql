092d848ef6a51b3fcd8839556f6d4a4e0a6c8aa7
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-02-26 21:23:50 +0100

    reformat nth_value.sql

--- SQL/select/analytical_functions/nth_value.sql
+++ SQL/select/analytical_functions/nth_value.sql
@@ -1,36 +1,51 @@
 create table tq84_nth_value (
-  part varchar2(3),
-  i    number,
-  a    varchar2(10)
+   i    number,
+   a    varchar2(10)
 );
 
-insert into tq84_nth_value values ('bar',  1, 'one'       );
-insert into tq84_nth_value values ('bar',  5, 'five'      );
-insert into tq84_nth_value values ('bar',  8, 'eight'     );
-insert into tq84_nth_value values ('bar',  9, 'nine'      );
-insert into tq84_nth_value values ('bar', 11, 'eleven'    );
-insert into tq84_nth_value values ('bar', 13, 'thirteen'  );
-
-insert into tq84_nth_value values ('baz',  2, 'two'       );
-insert into tq84_nth_value values ('baz',  3, 'three'     );
-insert into tq84_nth_value values ('baz',  4, 'four'      );
-
-insert into tq84_nth_value values ('foo',  7, 'seven'     );
-insert into tq84_nth_value values ('foo', 12, 'twelve'    );
-insert into tq84_nth_value values ('foo', 14, 'fourteen'  );
-insert into tq84_nth_value values ('foo', 20, 'twenty'    );
-insert into tq84_nth_value values ('foo', 21, 'twenty-one');
-insert into tq84_nth_value values ('foo', 22, 'twenty-two');
+insert into tq84_nth_value values (11, 'eleven'    );
+insert into tq84_nth_value values ( 3, 'three'     );
+insert into tq84_nth_value values (21, 'twenty-one');
+insert into tq84_nth_value values ( 8, 'eight'     );
+insert into tq84_nth_value values ( 9, 'nine'      );
+insert into tq84_nth_value values (22, 'twenty-two');
+insert into tq84_nth_value values (13, 'thirteen'  );
+insert into tq84_nth_value values ( 4, 'four'      );
+insert into tq84_nth_value values ( 1, 'one'       );
+insert into tq84_nth_value values ( 7, 'seven'     );
+insert into tq84_nth_value values (12, 'twelve'    );
+insert into tq84_nth_value values ( 5, 'five'      );
+insert into tq84_nth_value values (14, 'fourteen'  );
+insert into tq84_nth_value values ( 2, 'two'       );
+insert into tq84_nth_value values (20, 'twenty'    );
 
 select
-  part,
-  i
-  a,
-  nth_value(a, 3) from first over (partition by part order by i                                                          ) p,
-  nth_value(a, 3) from first over (partition by part order by i range between unbounded preceding and unbounded following) q,
-  nth_value(a, 3) from last  over (partition by part order by i                                                          ) r,
-  nth_value(a, 3) from last  over (partition by part order by i range between unbounded preceding and unbounded following) t
+-- i,
+   a,
+   nth_value(a, 3) from first over (order by i                                                          ) p,
+   nth_value(a, 3) from first over (order by i range between unbounded preceding and unbounded following) q,
+   nth_value(a, 3) from first over (order by i range between 4         preceding and unbounded following) r,
+   nth_value(a, 3) from last  over (order by i                                                          ) s,
+   nth_value(a, 3) from last  over (order by i range between unbounded preceding and unbounded following) t
 from
-  tq84_nth_value;
+   tq84_nth_value;
+--
+-- A          P          Q          R          S          T
+-- ---------- ---------- ---------- ---------- ---------- ----------
+-- one                   three      three                 twenty
+-- two                   three      three                 twenty
+-- three      three      three      three      one        twenty
+-- four       three      three      three      two        twenty
+-- five       three      three      three      three      twenty
+-- seven      three      three      five       four       twenty
+-- eight      three      three      seven      five       twenty
+-- nine       three      three      eight      seven      twenty
+-- eleven     three      three      nine       eight      twenty
+-- twelve     three      three      eleven     nine       twenty
+-- thirteen   three      three      twelve     eleven     twenty
+-- fourteen   three      three      thirteen   twelve     twenty
+-- twenty     three      three      twenty-two thirteen   twenty
+-- twenty-one three      three      twenty-two fourteen   twenty
+-- twenty-two three      three      twenty-two twenty     twenty
 
 drop table tq84_nth_value purge;
