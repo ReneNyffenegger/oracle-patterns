401a9a84d3a70f12e0b9b0ce33712c2157996a6d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-02-26 21:33:31 +0100

    reformat first_value.sql

--- SQL/select/analytical_functions/first_value.sql
+++ SQL/select/analytical_functions/first_value.sql
@@ -1,6 +1,6 @@
 create table tq84_first_value (
-  a number,
-  b varchar2(10)
+   a number  (2),
+   b varchar2(2)
 );
 
 insert into tq84_first_value values (1, 'B');
@@ -10,12 +10,22 @@ insert into tq84_first_value values (4, 'E');
 insert into tq84_first_value values (5, 'D');
 
 select
-  a,
-  b,
-  first_value(a) over (order by b) first_a, -- 3 (A is first)
-  first_value(b) over (order by a) first_b  -- B (1 is first)
+   a,
+   b,
+   first_value(a   ) over (order by b) first_a , -- 3 (A is first)
+   nth_value  (a, 1) over (order by b) first_a_, -- Same as above, but using nth_value
+   first_value(b   ) over (order by a) first_b , -- B (1 is first)
+   nth_value  (b, 1) over (order by a) first_b_  -- Same as above, but using nth_value
 from
-  tq84_first_value;
-
+   tq84_first_value;
+--
+-- 
+--   A B  FIRST_A FIRST_A_ FI FI
+-- --- -- ------- -------- -- --
+--   1 B        3        3 B  B 
+--   2 C        3        3 B  B 
+--   3 A        3        3 B  B 
+--   4 E        3        3 B  B 
+--   5 D        3        3 B  B 
 
 drop table tq84_first_value purge;
