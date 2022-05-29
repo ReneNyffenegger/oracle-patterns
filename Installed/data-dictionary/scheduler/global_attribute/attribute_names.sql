9a353f2e61e34d0d9427283c2319fbd07144b362
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-05-29 14:03:07 +0200

    Update attribute_names.sql

--- Installed/data-dictionary/scheduler/global_attribute/attribute_names.sql
+++ Installed/data-dictionary/scheduler/global_attribute/attribute_names.sql
@@ -1,18 +1,6 @@
 select
-  lower(attribute_name)
+   lower(attribute_name) name
 from
-  dba_scheduler_global_attribute
+   dba_scheduler_global_attribute
 order by
-  lower(attribute_name);
---
--- current_open_window
--- default_timezone
--- email_sender
--- email_server
--- email_server_credential
--- email_server_encryption
--- event_expiry_time
--- file_watcher_count
--- last_observed_event
--- log_history
--- max_job_slave_processes
+   lower(attribute_name);
