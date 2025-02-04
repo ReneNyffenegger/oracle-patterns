da4b5af73e9015021c902575339a53bfacba8312
Nyffenegger René <mail@renenyffenegger.ch>
2025-02-04 07:03:39 +0100

    move to notes

--- Installed/dynamic-performance-views/session/identify-own-session.sql
+++
@@ -1,4 +0,0 @@
--- The »own« session can be found with sys_context with two possibilities:
-
-select * from v$session where sid    = sys_context('userenv', 'sid'      );
-select * from v$session where audsid = sys_context('userenv', 'sessionid');
